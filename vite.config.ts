38b809b8fdc0e059ed0298d1f7e7e0eed684d9e4
Linekiller89 <wkdnfla14@gmail.com>
2025-04-26 13:22:00 +0900

    fix: update base URL for user pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/linekiller89.github.io/", // GitHub Pages 배포를 위한 base URL
+  base: "/", // GitHub 사용자 페이지를 위한 base URL
 });
